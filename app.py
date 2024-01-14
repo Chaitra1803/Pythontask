68c4e2e32868e2bb51f23ded1bcb8778ca77d9b7
Chaitra Teggi <48706140+Chaitra1803@users.noreply.github.com>
2024-01-14 14:34:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,29 +6,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')  # Render the template
+  return 'Hello, this is a sample Python Frontend Application!'
 
 @app.route('/datetime')
 def current_datetime():
-    now = datetime.now()
-    formatted_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
-    return f'Current Date and Time: {formatted_date_time}'
+  now = datetime.now()
+  formatted_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
+  return f'Current Date and Time: {formatted_date_time}'
 
 @app.route('/form', methods=['GET', 'POST'])
 def form_example():
-    if request.method == 'POST':
-        name = request.form['name']
-        return f'Thank you, {name}, for submitting the form!'
-    return '''
-        <h2>Sample Form</h2>
-        <form method="post" action="/form">
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required>
-            <br><br>
-            <input type="submit" value="Submit">
-        </form>
-    '''
+  if request.method == 'POST':
+    name = request.form['name']
+    return f'Thank you, {name}, for submitting the form!'
+  return '''
+    <h2>Sample Form</h2>
+    <form method="post" action="/form">
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" required>
+      <br><br>
+      <input type="submit" value="Submit">
+    </form>
+  '''
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
+  app.run(debug=True, host='0.0.0.0')
